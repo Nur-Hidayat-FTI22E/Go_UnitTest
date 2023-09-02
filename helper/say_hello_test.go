be5803903fe4ec6e2acef50fe50853c71c4255d6
Log-Hori'xins <133627973+Nur-Hidayat-FTI22E@users.noreply.github.com>
2023-09-02 16:23:31 +0000

    Unit Test

---
+++ helper/say_hello_test.go
@@ -0,0 +1,53 @@
+package helper
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "darwin" {
+		t.Skip("Can not run on Mac os bro")
+	}
+
+	result := SayHello("dayat")
+	require.Equal(t, "Hello dayat", result, "result must be 'Hello dayat'")
+}
+
+func TestSayHelloRequire(t *testing.T) {
+	result := SayHello("dayat")
+	require.Equal(t, "Hello dayat", result, "result must be 'Hello dayat'")
+	fmt.Println("TestSayHelloAssert with Assert Done")
+}
+func TestSayHelloAssert(t *testing.T) {
+	result := SayHello("dayat")
+	assert.Equal(t, "Hello dayat", result, "result must be 'Hello dayat'")
+	fmt.Println("TestSayHelloAssert with Assert Done")
+}
+
+func TestSayHello(t *testing.T) {
+	result := SayHello("dayat")
+
+	if result != "Hello dayat" {
+		// error
+		t.Error("Result is not 'Hello dayat'")
+	}
+
+	fmt.Println("halo bos")
+
+}
+func TestSayHelloccnt(t *testing.T) {
+	result := SayHello("ccnt")
+
+	if result != "Hello ccnt" {
+		// error
+		t.Fatal("Result is not 'Hello ccnt'")
+	}
+
+	fmt.Println("halo bosku")
+
+}
